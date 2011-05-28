ab66cd7d1c17fde9df0699e2a2c3e6d7c8b12941
David Zhang <dmonopoly10@gmail.com>
2011-05-28 13:31:41 -0400

    initial commit - up thru p330

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = 'ba4c8695c3caaa1c5a408988276e31a48cfe8a21930774071b6739987bfcd319c8363b37f2059033511b20734e7ce357803420c712d416b7e647216c6bf768cd'
