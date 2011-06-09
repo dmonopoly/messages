295b139452d27289f093069dca9b6c29adc5690e
David Zhang <dmonopoly10@gmail.com>
2011-06-08 23:17:59 -0400

    up to p332, about to add input fields

--- spec/views/messages/show.html.erb_spec.rb
+++ spec/views/messages/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "messages/show.html.erb" do
   it "displays the text attribute of the message" do
     assign :message, double("MessageAsISpecified", :text => "Hello world!")
-    render
+    render # uses the "messages/show.html.erb" from the outermost describe block
     rendered.should have_content("Hello world!")
   end
 end
