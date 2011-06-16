f9cde4ee1ad4ffad718b6b07d7b9f59e30eea25f
David Zhang <dmonopoly10@gmail.com>
2011-06-16 14:57:44 -0400

    message controller spec finished (thru p357)

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -15,15 +15,38 @@ describe MessagesController do
       post :create, :message => { "text" => "a quick brown fox" }
     end
 
-    it "saves the message" do
-      message.should_receive(:save)
-      post :create
+    context "when the message saves successfully" do
+      before do
+        message.stub(:save).and_return(true)
+        post :create
+      end
+
+      it "sets a flash[:notice] message" do
+        flash[:notice].should eq("The message was saved successfully.")
+      end
+
+      it "redirects to the Messages index" do
+        response.should redirect_to(:action => :index)
+      end
     end
 
-    it "redirects to the Messages index" do
-      post :create
-      response.should redirect_to(:action => :index)
+    context "when the message fails to save" do
+      before do
+        message.stub(:save).and_return(false)
+      end
+
+      it "assigns @message" do
+        post :create
+        assigns[:message].should eq(message)
+      end
+
+      it "renders the new template" do
+        post :create
+        response.should render_template(:new)
+      end
     end
+
   end
+
 end
 
