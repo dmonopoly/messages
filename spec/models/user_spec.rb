f54b29cd349fc0c3cbc826acf14a1d6dfefe94eb
David Zhang <dmonopoly10@gmail.com>
2011-06-23 01:15:51 -0400

    up to voila on p373

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe User do
+
+  describe "#send_message" do
+
+    context "when the user is under their subscription limit" do
+      it "sends a message to another user" do
+        zach = User.create!
+        david = User.create!
+        msg = zach.send_message(
+          :recipient => david,
+          :title => "Book Update",
+          :text => "Beta 11 includes great stuff!",
+          :recipient => david
+        )
+        david.received_messages.should == [msg]
+      end
+    end
+
+  end
+
+end
+
